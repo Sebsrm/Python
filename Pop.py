f8d21af3ba17684c8db91af8deaebee0e8ff3a68
Sebsrm <juansebastianramos89@gmail.com>
2025-05-12 17:00:39 -0500

    Add files via upload

---
+++ Pop.py
@@ -0,0 +1,25 @@
+vocales = ["a", "e", "i", "o", "u"]
+print(f"Lista: {vocales}")
+print(f"Elemento eliminado: {vocales.pop()}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLista: {vocales}")
+print(f"Elemento eliminado en la posicion 2: {vocales.pop(2)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLista: {vocales}")
+print(f"Elemento eliminado en la posicion 0: {vocales.pop(0)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLista: {vocales}")
+print(f"Elemento eliminado en la posicion -2: {vocales.pop(-2)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLista: {vocales}")
+print(f"Elemento eliminado en la posicion 5: {vocales.pop(5)}")
+print(f"Lista: {vocales}")
+#Esta linea de codigo no se ejecuta porque la posicion 5 no existe
