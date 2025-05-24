12bab49c613e8dc35e3f2b6693c34086399f84a7
Sebsrm <juansebastianramos89@gmail.com>
2025-05-24 13:10:36 -0500

    Add files via upload

---
+++ Desempaquetado_de_tuplas.py
@@ -0,0 +1,9 @@
+countries_tuple = ("Mexico", "Brasil", "Argentina", "España")
+print(countries_tuple, "\n")
+
+p1, p2, p3 = countries_tuple[:3]
+
+print("Desempaquetado de tupla [:3]")
+print(f"Primer pais: {p1}")
+print(f"Segundo pais: {p2}")
+print(f"Tercer pais: {p3}")
